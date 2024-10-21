239a2ddf9fffc5dd404ba616f75918a5b565f7a3
Chris Ware <c.p.ware@hotmail.com>
2024-10-20 23:11:33 -0700

    commit

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Add from "@/components/Add";
 import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
